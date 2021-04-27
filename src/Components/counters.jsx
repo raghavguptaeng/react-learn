182efd910688c04213661622eb0eb410a4d62b1e
raghav gupta <raghavguptaeng@users.noreply.github.com>
2021-04-27 07:08:55 +0530

    Tip Implemnented

--- src/Components/counters.jsx
+++ src/Components/counters.jsx
@@ -1,47 +1,14 @@
 import React, { Component } from 'react';
 import Raghav from './Title';
 class Counters extends Component {
-    state = { 
-        counters:this.props.counters
-     }
-    handleDlete = cid =>{
-        const newData = this.state.counters.filter(index => index.id !== cid);
-        this.setState(
-            {
-                counters:newData
-            }
-        );
-    } 
-
-    increment = (cid) =>{
-        var data = [...this.state.counters];
-        for(var i=0 ; i<this.state.counters.length ; ++i){
-            if(this.state.counters[i].id === cid){
-                data[i] = this.state.counters[i];
-                data[i].value++;
-            }
-            else{
-                data[i] = this.state.counters[i];
-            }
-        }
-        this.setState({counters:data});
-    }
-
-    reset=()=>{
-        var data = [];
-        for(var i=0 ; i<this.state.counters.length ; ++i){
-                data[i] = this.state.counters[i];
-                data[i].value = 0;
-        }
-        this.setState({counters:data});
-    }
 
     render() { 
+        const {del,inc,res,data} = this.props;
         return (
         <div>
-            <button className="btn btn-primary btn-m m-2" onClick={this.reset}>Reset</button>
-            {this.state.counters.map(counter =>
-                 <Raghav key={counter.id} onDlkuchbhinaam={this.handleDlete} increment={this.increment} counterData={counter}/>)}  
+            <button className="btn btn-primary btn-m m-2" onClick={res}>Reset</button>
+            {data.map(counter =>
+                 <Raghav key={counter.id} onDlkuchbhinaam={del} increment={inc} counterData={counter}/>)}  
         </div>
     );
     }
--- src/app.js
+++ src/app.js
@@ -9,12 +9,44 @@ class App extends Component {
         {id:3,value:6},
         {id:4,value:0}
     ] }
+    handleDlete = cid =>{
+        const newData = this.state.counters.filter(index => index.id !== cid);
+        this.setState(
+            {
+                counters:newData
+            }
+        );
+    } 
+
+    increment = (cid) =>{
+        var data = [...this.state.counters];
+        for(var i=0 ; i<this.state.counters.length ; ++i){
+            if(this.state.counters[i].id === cid){
+                data[i] = this.state.counters[i];
+                data[i].value++;
+            }
+            else{
+                data[i] = this.state.counters[i];
+            }
+        }
+        this.setState({counters:data});
+}
+
+    reset=()=>{
+        var data = [];
+        for(var i=0 ; i<this.state.counters.length ; ++i){
+                data[i] = this.state.counters[i];
+                data[i].value = 0;
+        }
+        this.setState({counters:data});
+    }
+
     render() { 
         return (  
             <React.Fragment>
                 <NavBar count={this.state.counters.length} />
                     <main className='container'>
-                        <Counters counters = {this.state.counters} />
+                        <Counters inc={this.increment} del={this.handleDlete} res={this.reset} data={this.state.counters} />
                     </main>
             </React.Fragment>
         );
